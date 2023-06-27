c3ee93043a989fbd980782fc5393123295dd0940
Annarhysa Albert <annarhysa13@gmail.com>
2023-06-27 19:43:03 +0530

    New Changes

--- app.py
+++ app.py
@@ -7,7 +7,67 @@ app = dash.Dash(__name__)
 
 server = app.server
 
-app.layout = html.Div([item, item2])
+app.layout = html.Div([item1, item2])
+
+html.Div(
+          [
+            html.P("Welcome to the Stock Dash App!", className="start"),
+            html.Div([
+              # stock code input
+            ]),
+            html.Div([
+              # Date range picker input
+            ]),
+            html.Div([
+              # Stock price button
+              # Indicators button
+              # Number of days of forecast input
+              # Forecast button
+            ]),
+          ],
+        className="nav")
+
+html.Div(
+          [
+            html.Div(
+                  [  # Logo
+                    # Company Name
+                  ],
+                className="header"),
+            html.Div( #Description
+              id="description", className="decription_ticker"),
+            html.Div([
+                # Stock price plot
+            ], id="graphs-content"),
+            html.Div([
+                # Indicator plot
+            ], id="main-content"),
+            html.Div([
+                # Forecast plot
+            ], id="forecast-content")
+          ],
+        className="content")       
+
+        html.Div(
+            [
+              html.Div(
+                    [  # Logo
+                      # Company Name
+                    ],
+                  className="header"),
+              html.Div( #Description
+                id="description", className="decription_ticker"),
+              html.Div([
+                  # Stock price plot
+              ], id="graphs-content"),
+              html.Div([
+                  # Indicator plot
+              ], id="main-content"),
+              html.Div([
+                  # Forecast plot
+              ], id="forecast-content")
+            ],
+          className="content")
 
 if __name__ == '__main__':
     app.run_server(debug=True)
