112cb7473ab8dca4d566b9998c7146d91d3ec25f
Annarhysa Albert <annarhysa13@gmail.com>
2023-06-28 22:06:48 +0530

    New Changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from datetime import datetime as dt
+import pandas_datareader.data as web
 
 app = dash.Dash(__name__)
 
@@ -25,3 +26,7 @@ app.layout = html.Div(children = [
 def update_value(input_data):
     start = dt.datetime(2010, 1, 1)
     end = dt.datetime.now()
+    df = web.DataReader(input_data, 'yahoo', start, end)
+    return dcc.Graph(id = "demo", figure ={'data': 
+    [{'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},],
+    'layout': {'title': input_data}})
